4f137e4d17db5cf932a00996115aa8fcd4a49df2
Maksim Torzhkov <maxtorj@yandex.ru>
2025-06-21 15:38:39 +0300

    feat: add User class, controller, exception package

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,36 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-@Data
+/**
+ * Film.
+ */
+@Getter
+@Setter
 public class Film {
-    private Set<Long> likes = new HashSet<>();
-    private Long id;
-
-    @NotBlank(message = "Name не может быть пустым")
-    @Size(max = 200)
-    private String name;
-
-    @Size(max = 200, message = "Описание не должно превышать 200 символов")
-    private String description;
-
-    @NotNull(message = "Дата релиза должна быть указана")
-    private LocalDate releaseDate;
-
-    @NotNull(message = "Продолжительность не может быть пустой")
-    @Positive(message = "Продолжительность должна быть положительной")
-    private Long duration;
-
-    //@NotEmpty(message = "Фильм должен относиться хотя бы к одному жанру")
-    private List<Genre> genres = new ArrayList<>();
-
-    @NotNull(message = "фильм должен иметь рейтинг")
-    private RatingMpa mpa;
 }
