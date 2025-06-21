2028ee1783f4af0e3d8df3a01282f4e5d7ced5b5
Maksim Torzhkov <maxtorj@yandex.ru>
2025-06-21 15:38:39 +0300

    feat: add User class, controller, exception package

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,9 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-/**
- * Исключение, выбрасываемое при ошибках валидации данных.
- * Используется для обозначения некорректного формата или значений входных данных.
- */
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,25 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
-/**
- * Класс, представляющий модель пользователя.
- * Содержит информацию о пользователе, включая имя, email, логин и дату рождения.
- */
 @Data
 public class User {
     private Long id;
-    private String name;
-    @NotNull(message = "Email не может быть пустым")
-    @Email(message = "Некорректный формат email")
+    @NonNull
     private String email;
-    @NotNull(message = "Логин не может быть пустым")
-    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
+    @NonNull
     private String login;
-    @NotNull(message = "Дата рождения не может быть пустой")
-    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    private String name;
+    @NonNull
     private LocalDate birthday;
 }
