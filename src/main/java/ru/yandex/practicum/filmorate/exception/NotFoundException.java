cbb370e4ba27fab4b0c4f22feca09cd3048fbd4b
Maksim Torzhkov <maxtorj@yandex.ru>
2025-06-21 16:56:03 +0300

    feat: add UserControllers, exceptions

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,9 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-/**
- * Исключение, выбрасываемое при попытке обратиться к данным, которые не существуют в системе.
- * Используется для обозначения отсутствия необходимых записей, таких как пользователь или фильм.
- */
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
