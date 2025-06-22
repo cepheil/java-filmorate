b69d05d83d0f403de5225e22e7f762b4cdd5fa4a
Maksim Torzhkov <maxtorj@yandex.ru>
2025-06-22 14:32:19 +0300

    feat: User and UserController refactoring

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
 public class User {
     private Long id;
+    @NonNull
     private String email;
+    @NonNull
     private String login;
     private String name;
+    @NonNull
     private LocalDate birthday;
 }
