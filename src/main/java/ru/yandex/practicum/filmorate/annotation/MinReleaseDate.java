5302c56c19bb8af484dc4ce8eb2bfe44dfbd9670
Максим Торжков <trzm@h.hermes-industries.local>
2025-06-23 15:34:33 +0300

    feat: add javaDoc comments

--- src/main/java/ru/yandex/practicum/filmorate/annotation/MinReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/MinReleaseDate.java
@@ -9,11 +9,27 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация {@code MinReleaseDate} используется для валидации даты релиза фильма.
+ * Она гарантирует, что дата не может быть раньше установленной минимальной даты (28 декабря 1895 года).
+ */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = MinReleaseDateValidator.class)
 public @interface MinReleaseDate {
+    /**
+     * Возврат сообщения об ошибке, если дата релиза меньше допустимой.
+     * @return сообщение об ошибке
+     */
     String message() default "Дата релиза не может быть раньше 28 декабря 1895 года";
+    /**
+     * Группы валидации, к которым относится эта аннотация.
+     * @return массив групп валидации
+     */
     Class<?>[] groups() default {};
+    /**
+     * Дополнительные данные (payload), связанные с ограничением.
+     * @return массив объектов Payload
+     */
     Class<? extends Payload>[] payload() default {};
 }
