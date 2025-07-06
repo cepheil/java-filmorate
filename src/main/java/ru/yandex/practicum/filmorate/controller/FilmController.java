81ad476e7e1dd65f54c2dec3f475d7e1cdb4190b
Maksim Torzhkov <maxtorj@yandex.ru>
2025-07-06 11:18:58 +0300

    feat: add Controllers logic

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,107 +1,51 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
+import java.util.Collection;
 
-/**
- * Контроллер для управления фильмами.
- * Обеспечение получения, создания и обновления списка фильмов.
- */
 @Slf4j
 @RestController
 @RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
+    private final FilmStorage filmStorage;
+    private final FilmService filmService;
 
-    private final Map<Long, Film> films = new HashMap<>();
-    private final AtomicLong idCounter = new AtomicLong(1);
-
-    /**
-     * Возвращение списка всех фильмов, отсортированных по ID.
-     * @return коллекция фильмов в порядке возрастания ID.
-     */
     @GetMapping
     public Collection<Film> findAllFilms() {
-        log.info("GET /films - получение списка всех фильмов");
-        return films.values()
-                .stream()
-                .sorted(Comparator.comparing(Film::getId))
-                .collect(Collectors.toList());
+        return filmStorage.findAllFilms();
     }
 
-    /**
-     * Создание нового фильма.
-     * Валидация данных фильма.
-     * @param film данные фильма для создания.
-     * @return созданный фильм с присвоенным ID.
-     * @throws ValidationException если данные фильма не проходят валидацию.
-     */
+    @GetMapping
+    public Collection<Film> getPopulateFilms(@RequestParam(defaultValue = "10") int count) {
+        return filmService.getPopularFilms(count);
+    }
+
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
-        log.info("POST /films - попытка добавления фильма: {}", film.getName());
-        try {
-            if (films.values().stream().anyMatch(f -> f.getName().equalsIgnoreCase(film.getName()))) {
-                log.error("Такое название уже существует: {}", film.getName());
-                throw new DuplicatedDataException("Фильм с таким названием уже существует");
-            }
-            film.setId(getNextId());
-            films.put(film.getId(), film);
-            log.info("Фильм создан: ID={}", film.getId());
-            return film;
-        } catch (RuntimeException e) {
-            log.error("Ошибка при создании фильма", e);
-            throw e;
-        }
+        return filmStorage.createFilm(film);
     }
 
-    /**
-     * Обновление данных существующего фильма.
-     * Проверка, что фильм существует и название не дублируется.
-     * @param newFilm новые данные фильма.
-     * @return обновленный фильм.
-     * @throws NotFoundException если фильм с указанным ID не найден.
-     * @throws DuplicatedDataException если новое название уже используется.
-     * @throws ValidationException если данные не проходят валидацию.
-     */
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film newFilm) {
-        log.info("PUT /films - попытка обновления фильма {}", newFilm.getName());
-        try {
-            Film existingFilm = films.get(newFilm.getId());
-            if (existingFilm == null) {
-                log.error("Отсутствует фильм с ID: {}", newFilm.getId());
-                throw new NotFoundException("Фильм с ID " + newFilm.getId() + " не найден");
-            }
-            if (!newFilm.getName().equalsIgnoreCase(existingFilm.getName())
-            && films.values().stream().anyMatch(film -> film.getName().equalsIgnoreCase(newFilm.getName()))) {
-                log.error("Такое название уже существует: {}", newFilm.getName());
-                throw new DuplicatedDataException("Фильм с таким названием уже существует");
-            }
-            existingFilm.setName(newFilm.getName());
-            Optional.ofNullable(newFilm.getDescription()).ifPresent(existingFilm::setDescription);
-            Optional.ofNullable(newFilm.getReleaseDate()).ifPresent(existingFilm::setReleaseDate);
-            Optional.ofNullable(newFilm.getDuration()).ifPresent(existingFilm::setDuration);
-            return existingFilm;
-        } catch (RuntimeException e) {
-            log.error("Ошибка при обновлении фильма", e);
-            throw e;
-        }
+        return filmStorage.updateFilm(newFilm);
     }
 
-    /**
-     * Генерация следующего ID для нового фильма.
-     * Поиск максимально-существующего ID и увеличение его на 1.
-     * @return следующий доступный ID.
-     */
-    private long getNextId() {
-        return idCounter.getAndIncrement();
+    @PutMapping("/{filmId}/like/{userId}")
+    public void addLike(@PathVariable Long filmId,
+                        @PathVariable Long userId) {
+        filmService.addLike(filmId, userId);
+    }
+    @DeleteMapping("/{filmId}/like/{userId}")
+    public void removeLike(@PathVariable Long filmId,
+                           @PathVariable Long userId) {
+        filmService.removeLike(filmId, userId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,118 +1,59 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
-/**
- * Контроллер для управления пользователями.
- * Обеспечение получения, создания и обновления списка пользователей.
- */
 @Slf4j
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserStorage userStorage;
+    private final UserService userService;
 
-    private final Map<Long, User> users = new HashMap<>();
-    private final AtomicLong idCounter = new AtomicLong(1);
-
-    /**
-     * Возвращение списка всех пользователей, отсортированных по ID.
-     * @return коллекция пользователей в порядке возрастания ID.
-     */
     @GetMapping
     public Collection<User> findAllUsers() {
-        log.info("GET /users - получение списка всех пользователей");
-        return users.values()
-                .stream()
-                .sorted(Comparator.comparing(User::getId))
-                .collect(Collectors.toList());
+        return userStorage.findAllUsers();
     }
 
-    /**
-     * Создание нового пользователя.
-     * Валидация данных пользователя. Если имя пустое, заменяет его логином.
-     * @param user данные пользователя для создания.
-     * @return созданный пользователь с присвоенным ID.
-     * @throws ValidationException если данные пользователя не проходят валидацию.
-     */
+    @GetMapping("/{friendId}/friends")
+    public Collection<User> getFriends(@PathVariable Long friendId) {
+        return userService.getFriends(friendId);
+    }
+
+    @GetMapping("/{friendId}/friends/common/{userId}")
+    public Collection<User> getCommonFriends(@PathVariable Long friendId,
+                                             @PathVariable Long userId) {
+        return userService.getCommonFriends(friendId, userId);
+    }
+
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-        log.info("POST /users - попытка создания пользователя: {}", user.getEmail());
-        try {
-            if (users.values().stream().anyMatch(u -> u.getEmail().equalsIgnoreCase(user.getEmail()))) {
-                log.error("Такой email уже существует: {}", user.getEmail());
-                throw new DuplicatedDataException("Email уже используется");
-            }
-            user.setId(getNextId());
-            if (user.getName() == null || user.getName().isBlank()) {
-                user.setName(user.getLogin());
-                log.debug("Замена пустого имени на логин: {}", user.getLogin());
-            }
-            users.put(user.getId(), user);
-            log.info("Пользователь создан: ID={}", user.getId());
-            return user;
-        } catch (RuntimeException e) {
-            log.error("Ошибка при создании пользователя", e);
-            throw e;
-        }
+        return userStorage.createUser(user);
     }
 
-    /**
-     * Обновление данных существующего пользователя.
-     * Проверка, что пользователь существует и email не дублируется.
-     * @param newUser новые данные пользователя.
-     * @return обновленный пользователь.
-     * @throws NotFoundException если пользователь с указанным ID не найден.
-     * @throws DuplicatedDataException если новый email уже используется.
-     * @throws ValidationException если данные не проходят валидацию.
-     */
     @PutMapping
-    public User updateUser(@Valid @RequestBody User newUser) {
-        log.info("PUT /users - попытка обновления пользователя: {}", newUser.getEmail());
-        try {
-            User existingUser = users.get(newUser.getId());
-            if (existingUser == null) {
-                log.error("Отсутствует ID: {}", newUser.getId());
-                throw new NotFoundException("Пользователь с ID " + newUser.getId() + " не найден");
-            }
-            if (!newUser.getEmail().equals(existingUser.getEmail())
-                    && users.values().stream().anyMatch(user -> user.getEmail().equals(newUser.getEmail()))) {
-                log.error("Такой email уже существует: {}", newUser.getEmail());
-                throw new DuplicatedDataException("Email уже используется");
-            }
-            existingUser.setEmail(newUser.getEmail());
-            existingUser.setLogin(newUser.getLogin());
-            existingUser.setBirthday(newUser.getBirthday());
-            if (newUser.getName() != null && !newUser.getName().isBlank()) {
-                existingUser.setName(newUser.getName());
-            } else {
-                existingUser.setName(newUser.getLogin());
-            }
-            return existingUser;
-        } catch (RuntimeException e) {
-            log.error("Ошибка при обновлении пользователя", e);
-            throw e;
-        }
+    public User updateUser(@Valid @RequestBody User user) {
+        return userStorage.updateUser(user);
     }
 
-    /**
-     * Генерация следующего ID для нового пользователя.
-     * Поиск максимально-существующего ID и увеличение его на 1.
-     * @return следующий доступный ID.
-     */
-    private long getNextId() {
-        return idCounter.getAndIncrement();
+    @PutMapping("/{userId}/friends/{friendId}")
+    public void addFriend(@PathVariable Long userId,
+                          @PathVariable Long friendId) {
+        userService.addFriend(userId, friendId);
+    }
+
+    @DeleteMapping("/{userId}/friends/{friendId}")
+    public void removeFriend(@PathVariable Long userId,
+                             @PathVariable Long friendId) {
+        userService.removeFriend(userId, friendId);
     }
 }
+
