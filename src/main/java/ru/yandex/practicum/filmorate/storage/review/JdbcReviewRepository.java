76a569782d668b7806250de909c41cdc7d2f9c37
Maksim Torzhkov <maxtorj@yandex.ru>
2025-08-20 08:47:39 +0300

    feat: add review row mapper

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/JdbcReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/JdbcReviewRepository.java
@@ -1,4 +1,9 @@
 package ru.yandex.practicum.filmorate.storage.review;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Qualifier("reviewRepository")
 public class JdbcReviewRepository {
 }
